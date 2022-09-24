f1b776f43281e6564e06ab8dfd443c06efe3a994
Bn22 <bernardontoultimate@gmail.com>
2022-09-23 21:08:49 -0300

    add sorteionatal/sorteio.py

---
+++ sorteionatal/main.py
@@ -0,0 +1,11 @@
+import random
+
+print('***********\nFELIZ NATAL\n***********')
+
+
+nome = ['Eduardo', 'Gustavo', 'Amora', 'Bernardo', 'Raul']
+while len(nome) != 0:print(a := random.randint(0, len(nome)-1),end='\r'), print(nome[a]),nome.pop(a)
+
+
+
+
